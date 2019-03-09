62482812481c87b47a6960e130e3d821610a9473
Jimmy <jimmyskripchuk@gmail.com>
2019-03-09 15:24:14 -0500

    added tree builder, and tree output

--- src/main.py
+++ src/main.py
@@ -35,9 +35,77 @@ def main():
 	test = pandas.read_csv(test)
 	valid = pandas.read_csv(valid)
 
-	best_split(train, entropy)
+	root = Node()
+	build_tree(root, train)
 
+	print_subtree(root,0)
+
+#Left branches are attribute values of 0
+#Right branches are attribute values of 1
+
+#Slow, I think I may be copying data down each split of the tree then
+#just filtering it like I want to
+def build_tree(node, data):
+	#At each layer, filter the data so that it only sees data that should be at that branch
+	filtered_subset = data
+	for key in node.previous_splits:
+		filtered_subset = filtered_subset[filtered_subset[key] == node.previous_splits[key]]
 	
+	#If this subset is pure, we don't need to split anymore
+	if entropy(filtered_subset) == 0:
+		if not filtered_subset.empty:
+			node.class_value = filtered_subset.iloc[0]["Class"]
+		else:
+			node.class_value = None
+		return 0
+	else:
+		#Get the best attribute to split on
+		best_attr = best_split(filtered_subset, entropy)
+		node.split_attribute = best_attr
+		
+		#Partition the left and right nodes
+		node.left = Node(node.previous_splits.copy())
+		node.left.previous_splits[best_attr] = 0
+
+		node.right = Node(node.previous_splits.copy())
+		node.right.previous_splits[best_attr] = 1
+
+		#Recurse!
+		build_tree(node.left, filtered_subset)
+		build_tree(node.right, filtered_subset)
+
+class Node:
+	def __init__(self, previous_splits={}):
+		self.left = None
+		self.right = None
+		self.split_attribute = None
+		self.previous_splits = previous_splits
+		self.class_value = None
+
+def print_subtree(node, indent):
+	if node != None and node.split_attribute != None:
+		left_subtree_print = ""
+		right_subtree_print = ""
+
+		for i in range(0, indent):
+			left_subtree_print+="| "
+			right_subtree_print+="| "
+
+
+		left_subtree_print += node.split_attribute+" = 0: "
+		if node.left.split_attribute == None:
+			left_subtree_print += str(node.left.class_value)
+
+		print(left_subtree_print)
+		print_subtree(node.left,indent+1)
+
+		right_subtree_print += node.split_attribute+" = 1: "
+		if node.right.split_attribute == None:
+			right_subtree_print += str(node.right.class_value)
+
+		print(right_subtree_print)
+
+		print_subtree(node.right,indent+1)
 
 # Given a subset and a heuristic, returns the attribute that has
 # the greatest info gain
