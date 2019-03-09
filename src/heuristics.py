6bd10ce1c93c85ee87816b5acd1e49abbcd5d277
Jason <jkr@udel.edu>
2019-03-08 16:24:14 -0800

    added variance impurity function to heuristics.py

--- src/heuristics.py
+++ src/heuristics.py
@@ -3,3 +3,14 @@ def info_gain(data, attr):
 
 def entropy(data):
 	pass
+
+
+def varianceImpurity(data):
+	counts = data['class'].value_counts()
+	k0 = counts[0]
+	k1 = counts[1]
+	k = data.count() 	
+
+	vi = (k0/k)*(k1/k)
+
+	return vi
