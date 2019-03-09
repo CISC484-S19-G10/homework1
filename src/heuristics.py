cfdeb41a20781377ca3b29342cebd805fcf62cfc
Joy Kitson <tkitson@udel.edu>
2019-03-08 19:21:29 -0500

    now calculating entropy on a given dataframe

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,5 +1,16 @@
+CLASS_COL = 'Class'
+
+import math
+
 def info_gain(data, attr):
 	pass
 
 def entropy(data):
-	pass
+	#get the counts of each class, i
+	counts = data[CLASS_COL].value_counts()
+
+	#divide by the number of rows to get the proportion, p_i, for each value i
+	proportions = 1.0 * counts / data.shape[0]
+
+	#then sum -p_i * log_2(p_i) for each i
+	return sum(map(lambda p: -p*math.log(p, 2), proportions))
