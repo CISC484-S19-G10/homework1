324f6efd8520a58864ce04a749729abd0118b438
Joy Kitson <tkitson@udel.edu>
2019-03-08 19:31:29 -0500

    dividing after zero check became of rounding

--- src/heuristics.py
+++ src/heuristics.py
@@ -9,12 +9,10 @@ def entropy(data):
 	#get the counts of each class, i
 	counts = data[CLASS_COL].value_counts()
 
-	#divide by the number of rows to get the proportion, p_i, for each value i
-	proportions = 1.0 * counts / data.shape[0]
-	
 	#then sum -p_i * log_2(p_i) for each i
 	total = 0
-	for p in proportions:
-		if p != 0:	
+	for c in counts:
+		if c != 0:
+			p = c / data.shape[0]
 			total += -p * math.log(p, 2)
 	return total
